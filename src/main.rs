f763a720bd67b5d777f454057be0d5abe7112eec
futa-t <143526236+futa-t@users.noreply.github.com>
2025-02-04 01:52:07 +0900

    ビルドバージョン更新いったんできた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::fs::*;
+use std::io::*;
+
+fn increment_build_number() -> usize {
+    let f_build = "buildversion";
+
+    let mut version = String::new();
+
+    match File::open(f_build) {
+        Ok(mut f) => {
+            let r = f.read_to_string(&mut version);
+            if let Err(_) = r {
+                version = "0".to_string();
+            }
+        }
+        Err(_) => {
+            version = "0".to_string();
+        }
+    }
+
+    let mut version = version.parse::<usize>().unwrap_or(0usize);
+    version += 1;
+
+    match File::create(f_build) {
+        Ok(f) => {
+            let mut bf = BufWriter::new(f);
+            bf.write(version.to_string().as_bytes()).unwrap();
+        }
+        Err(e) => println!("{}", e.to_string()),
+    }
+    return version;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", increment_build_number());
 }
